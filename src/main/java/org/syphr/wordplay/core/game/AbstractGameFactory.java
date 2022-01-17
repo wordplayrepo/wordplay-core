8e3a059a50c5356cdd9b092b0ab29bb459ef8b18
Gregory Moyer <moyerg@syphr.com>
2022-01-17 12:45:03 -0500

    Move XML configuration to separate library

--- src/main/java/org/syphr/wordplay/core/game/AbstractGameFactory.java
+++ src/main/java/org/syphr/wordplay/core/game/AbstractGameFactory.java
@@ -15,36 +15,14 @@
  */
 package org.syphr.wordplay.core.game;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.syphr.wordplay.core.config.Configuration;
-import org.syphr.wordplay.core.config.Configurations;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public abstract class AbstractGameFactory implements GameFactory
 {
     @Override
     public Game create()
     {
-        return create((Configuration) null);
-    }
-
-    @Override
-    public Game create(File configurationFile) throws IOException
-    {
-        log.trace("Creating new game from configuration at \"{}\"", configurationFile.getAbsolutePath());
-        return create(Configurations.create(Configurations.read(configurationFile)));
-    }
-
-    @Override
-    public Game create(InputStream configurationStream) throws IOException
-    {
-        log.trace("Creating game from configuration stream");
-        return create(Configurations.create(Configurations.read(configurationStream)));
+        return create(null);
     }
 
     @Override
--- src/main/java/org/syphr/wordplay/core/game/GameFactory.java
+++ src/main/java/org/syphr/wordplay/core/game/GameFactory.java
@@ -15,19 +15,11 @@
  */
 package org.syphr.wordplay.core.game;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.syphr.wordplay.core.config.Configuration;
 
 public interface GameFactory
 {
     Game create();
-
-    Game create(File configurationFile) throws IOException;
-
-    Game create(InputStream configurationStream) throws IOException;
 
     Game create(Configuration configuration);
 }
